e65f8f3c462895d2a66afef81ee160d93856d9aa
Elijah Arhinful <elijaharhinful8@gmail.com>
2024-07-07 15:31:50 +0000

    changes to the users service and controller

--- src/controllers/user.contoller.ts
+++ src/controllers/user.contoller.ts
@@ -1,25 +1,26 @@
 import { Request, Response } from "express";
 import { UserService } from "../services/user.service";
-import { OrganisationService } from "../services/organisation.service";
 
 export const UserController = {
   getUser: async (req: Request, res: Response) => {
     const { id } = req.params;
-    const requestingUser = req.user as any; // Type assertion, assuming req.user is set by authMiddleware
+    const requestingUser = req.user as any;
 
     try {
       let user;
       if (id === requestingUser.userId) {
+        // Get own record
         user = await UserService.getUserById(id);
       } else {
-        const orgsOwned = await UserService.getUserById(requestingUser.userId);
-        const orgIds = orgsOwned?.organisations.map(org => org.orgId) || [];
-        
-        for (const orgId of orgIds) {
-          const usersInOrg = await UserService.getUsersInOrganisation(orgId);
-          user = usersInOrg.find(u => u.userId === id);
-          if (user) break;
-        }
+        // Get user from organizations the requesting user belongs to or created
+        // const orgsOwned = await UserService.getUserById(requestingUser.userId);
+        // const orgIds = orgsOwned?.organisations.map(org => org.orgId) || [];
+        user = await UserService.getUserInSharedOrganizations(requestingUser.userId, id);
+        // for (const orgId of orgIds) {
+        //   const usersInOrg = await UserService.getUsersInOrganisation(orgId);
+        //   user = usersInOrg.find(u => u.userId === id);
+        //   if (user) break;
+        // }
       }
 
       if (!user) {
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -16,5 +16,15 @@ export const UserService = {
       .innerJoin("user.organisations", "org")
       .where("org.orgId = :orgId", { orgId })
       .getMany();
+  },
+  
+  getUserInSharedOrganizations: async (requestingUserId: string, targetUserId: string): Promise<User | null> => {
+    const userRepository = connectionSource.getRepository(User);
+    return await userRepository.createQueryBuilder("user")
+      .innerJoin("user.organisations", "org")
+      .innerJoin("org.users", "member")
+      .where("user.userId = :targetUserId", { targetUserId })
+      .andWhere("member.userId = :requestingUserId", { requestingUserId })
+      .getOne();
   }
 };
