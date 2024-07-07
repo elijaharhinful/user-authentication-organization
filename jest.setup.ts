ea6392a339e8d37e56c970b651af936ed610384c
Elijah Arhinful <elijaharhinful8@gmail.com>
2024-07-07 17:24:13 +0000

    changes to configs

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,4 +1,6 @@
 import { connectionSource } from "./src/database/ormconfig";
+import 'reflect-metadata'; // Ensure reflect-metadata is imported for TypeORM
+import { afterAll, afterEach, beforeAll, beforeEach, describe, expect, it } from '@jest/globals';
 
 afterAll(async () => {
   if (connectionSource.isInitialized) {
