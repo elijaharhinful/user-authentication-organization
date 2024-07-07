088ab14f6c423be89dc985c8ba4da0d828bbe877
Elijah Arhinful <elijaharhinful8@gmail.com>
2024-07-07 18:57:50 +0000

    tsconfig changes and decalaration file

---
+++ declarations.d.ts
@@ -0,0 +1,7 @@
+declare module 'express';
+declare module 'jsonwebtoken';
+declare module 'cors';
+declare module 'morgan';
+declare module 'swagger-ui-express';
+declare module 'bcryptjs';
+declare module 'swagger-jsdoc';
