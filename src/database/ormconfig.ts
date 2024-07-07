c1337d26ee53380d426e90eafec2984b2a89b807
Elijah Arhinful <elijaharhinful8@gmail.com>
2024-07-07 16:26:49 +0000

    update for render re-run

--- src/database/ormconfig.ts
+++ src/database/ormconfig.ts
@@ -5,19 +5,33 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// for production
 export const connectionSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST,
-  port: parseInt(process.env.DB_PORT || "5432"),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  url: process.env.DATABASE_URL,
   synchronize: false, // Set this to false in production
   logging: false,
   entities: [User, Organisation],
   migrations: ["dist/migrations/*.js"],
   subscribers: [],
-  ssl: process.env.NODE_ENV === 'production' ? {
-    rejectUnauthorized: false
-  } : false,
+  ssl: {rejectUnauthorized: false}
 });
+
+
+//for dev
+// export const connectionSource = new DataSource({
+//   type: "postgres",
+//   host: process.env.DB_HOST,
+//   port: parseInt(process.env.DB_PORT || "5432"),
+//   username: process.env.DB_USERNAME,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   synchronize: false, // Set this to false in production
+//   logging: false,
+//   entities: [User, Organisation],
+//   migrations: [],
+//   subscribers: [],
+//   ssl: process.env.NODE_ENV === 'production' ? {
+//     rejectUnauthorized: false
+//   } : false,
+// });
