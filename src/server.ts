b03bf757a03886381d296beddd9e308b987c4a30
Elijah Arhinful <elijaharhinful8@gmail.com>
2024-07-09 14:40:49 +0000

    changed routes to manual routing to solve 404 error

--- src/server.ts
+++ src/server.ts
@@ -7,10 +7,12 @@ import morgan from "morgan";
 import { readdirSync } from "fs";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./swagger";
-const authRoutes = require("./routes/auth.routes"); // Import auth routes explicitly
+import authRoutes from "./routes/auth.routes"; 
+import organisationRoutes from "./routes/organisation.routes"; 
+import userRoutes from "./routes/user.routes";
 
 export const app: Application = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // middleware setup
 app.use(express.json());
@@ -35,19 +37,29 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Serve all other routes dynamically using readdirSync
 const isProduction = process.env.NODE_ENV === 'production';
-const fileExtension = isProduction ? '.js' : '.ts';
+// const fileExtension = isProduction ? '.js' : '.ts';
 
 // Use dynamic import for auth routes
-import(`./routes/auth.routes${fileExtension}`).then((authRoutes) => {
-  app.use("/auth", authRoutes.default || authRoutes);
-});
+// import(`./routes/auth.routes${fileExtension}`).then((authRoutes) => {
+//   app.use("/auth", authRoutes.default || authRoutes);
+// });
 
-readdirSync("./src/routes").forEach((file) => {
-  if (file.endsWith(fileExtension) && file !== `auth.route${fileExtension}`) { // Exclude auth.route.ts
-    const route = require(`./routes/${file}`);
-    app.use("/api", route.default || route);
-  }
-});
+// const files = readdirSync("./src/routes");
+// console.log("All files in routes directory:", files);
+
+// files.forEach((file) => {
+//   console.log(`Checking file: ${file}`);
+//   if (file.endsWith(fileExtension) && file !== `auth.route${fileExtension}`) {
+//     console.log(`Registering route: ${file}`);
+//     const route = require(`./routes/${file}`);
+//     app.use("/api", route.default || route);
+//   }
+// });
+
+// Apply auth routes
+app.use("/auth", authRoutes);
+app.use("/api", organisationRoutes);
+app.use("/api", userRoutes);
 
 // Simple GET endpoint
 app.get('/', (req, res) => {
